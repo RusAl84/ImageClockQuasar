07eb5558f313e21a80965a644358f00c2b8dd9a6
Aleksey <rusal@bk.ru>
2022-02-05 00:33:51 +0300

    init

---
+++ src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-uploader
+      url="http://192.168.88.17:5000/upload"
+      label="Upload files"
+      color="purple"
+      square
+      flat
+      bordered
+      style="max-width: 300px"
+    />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+});
+</script>
